20b5bd699fdca3780d589645ab64564d6e09fe8b
Artur.Olekh <valeralarin301@gmail.com>
2024-05-26 19:16:24 +0300

    initial commit. New one

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(generateWord(8));
+    }
+    public static String generateWord(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append((char) ThreadLocalRandom.current().nextInt('a', 'z' + 1));
+        }
+        return sb.toString();
     }
 }
